4bb183d7a3525fe080451d80a4ccc3fb816dbb4f
jansulja <shuky11@rocketmail.com>
2016-05-18 19:46:07 +0200

    asd

--- WebContent/scripts/factories/gradjaninFactory.js
+++ WebContent/scripts/factories/gradjaninFactory.js
@@ -1,7 +1,7 @@
 angular.module('resource.gradjani', ['ngResource'])
 	.factory('Gradjanin', function ($resource,$log) {
 		$log.info('INSERT at: ' + new Date());	
-	return $resource('http://localhost:8080/xws/api/gradjanin/:gradjaninId',null, {
+	return $resource('http://localhost:8089/xws/api/gradjanin/:gradjaninId',null, {
         'update': { method:'PUT' }
     });
 })
