a61696537cddf35a55b4282650ee9498b2def596
jansulja <shuky11@rocketmail.com>
2016-05-18 20:07:07 +0200

    asd

--- WebContent/scripts/factories/invoiceFactory.js
+++
@@ -1,6 +0,0 @@
-angular.module('resource.invoice', ['ngResource'])
-	.factory('Invoice', function ($resource) {
-	return $resource('http://localhost:8089/xws/api/invoice/:invoiceId',null, {
-        'update': { method:'PUT' }
-    });
-})
