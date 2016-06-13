3256220ebc3448e57496af02106fc64ee120ea88
glowe9 <rhcpfilip@gmail.com>
2016-06-13 22:08:57 +0200

    asd

--- WebContent/scripts/controllers/akt-list.js
+++ WebContent/scripts/controllers/akt-list.js
@@ -3,12 +3,17 @@
 angular.module('akt-list',[])
 
 .controller('akt-listCtrl', function ($scope, $q, $http){
+		
+		$scope.Upit = {Naziv: "", ID: "", Status: "" , DatumPredlogaOd: "" , DatumPredlogaDo: "", DatumUsvajanja : "" , Predlagac: ""};
+		$scope.names = ["USVOJEN", "ODBIJEN", "PREDLOZEN","U_PROCESU"];
+		
 		var deferred = $q.defer();
 		$scope.akts = [];
 
 		$http({
 		  method: 'GET',
-		  url: 'https://localhost:8443/xws/api/akt/list'
+		  url: 'https://localhost:8443/xws/api/akt/list',
+		  headers: { "Content-Type": 'application/json' }
 		}).success(function (data) {
 		    // this callback will be called asynchronously
 		    // when the response is available
@@ -28,7 +33,64 @@ angular.module('akt-list',[])
 		});
 
 
+	//funkcija koja otvara datepicker
+	$scope.openDatepicker1 = function($event, opened) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope[opened] = true;
+	};
+
+	$scope.openDatepicker2 = function($event, opened) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope[opened] = true;
+	};
+
+		$scope.openDatepicker3 = function($event, opened) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope[opened] = true;
+	};
+
+$scope.searchAktove = function() {
+			var deferred = $q.defer();
+			
+			$http({
+				url: "https://localhost:8443/xws/api/akt/provera", 
+				method: "POST",
+				data: $scope.Upit
+
+			}).success(function (data) {
+				deferred.resolve(data);
+			});
+
+			var promise = deferred.promise;
+			promise.then(function (data) {
+				console.log('success');
+			});
+
+	};
+
+
+
 $scope.obrisiAkt = function (akt){
+     
+		var deferred = $q.defer();
+
+				$http({
+				url: "https://localhost:8443/xws/api/akt/delete", 
+				method: "POST",
+				data: akt
+			}).success(function (data) {
+				deferred.resolve(data);
+			});
+
+			var promise = deferred.promise;
+			promise.then(function (data) {
+
+				
+			});
+
 }
 	
 
